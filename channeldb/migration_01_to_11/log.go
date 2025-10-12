06f52301994f0962606affd8af1def11b50bc6f6
mechion <4387895+mechion@users.noreply.github.com>
2025-10-12 15:24:47 +0000

    chore: add log.go

---
+++ channeldb/migration_01_to_11/log.go
@@ -0,0 +1,14 @@
+package migration_01_to_11
+
+import (
+	flog "github.com/flokiorg/go-flokicoin/log/v2"
+)
+
+// log is a logger that is initialized as disabled.  This means the package will
+// not perform any logging by default until a logger is set.
+var log = flog.Disabled
+
+// UseLogger uses a specified Logger to output package logging info.
+func UseLogger(logger flog.Logger) {
+	log = logger
+}
