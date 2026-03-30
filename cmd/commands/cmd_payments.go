74d8cd8078b04c9588f088ec500788e267ad4188
Jaewook Lee <a@pororo.ro>
2026-03-30 23:27:15 +0900

    cmd: add outgoing_chan_id flag to estimatefee

--- cmd/commands/cmd_payments.go
+++ cmd/commands/cmd_payments.go
@@ -1968,6 +1968,15 @@ var estimateRouteFeeCommand = cli.Command{
 				"applicable if pay_req is specified.",
 			Value: paymentTimeout,
 		},
+		cli.StringSliceFlag{
+			Name: "outgoing_chan_id",
+			Usage: "short channel id of the outgoing channel " +
+				"to use for the first hop of the fee " +
+				"estimation; if specified multiple " +
+				"times, only the listed channels are " +
+				"considered for the first hop",
+			Value: &cli.StringSlice{},
+		},
 	},
 }
 
@@ -2012,6 +2021,15 @@ func estimateRouteFee(ctx *cli.Context) error {
 
 	default:
 		return fmt.Errorf("fee estimation arguments missing")
+	}
+
+	var (
+		err        error
+		outChanIDs = ctx.StringSlice("outgoing_chan_id")
+	)
+	req.OutgoingChanIds, err = parseChanIDs(outChanIDs)
+	if err != nil {
+		return fmt.Errorf("unable to decode outgoing_chan_id: %w", err)
 	}
 
 	resp, err := client.EstimateRouteFee(ctxc, req)
