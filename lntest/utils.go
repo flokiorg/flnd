lightningnetwork/lnd

itest: fix endorsement itests
Due to the signaling period expiring tests had to be adopted bc
they were not taking the activation time period into account.

lntest/utils.go
@@ -7,9 +7,11 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/wire"
+	"github.com/lightningnetwork/lnd"
 	"github.com/lightningnetwork/lnd/input"
 	"github.com/lightningnetwork/lnd/lnrpc"
 	"github.com/lightningnetwork/lnd/lntest/wait"
@@ -288,11 +290,26 @@ func CalcStaticFeeBuffer(c lnrpc.CommitmentType, numHTLCs int) btcutil.Amount {
 func CustomRecordsWithUnendorsed(
 	originalRecords lnwire.CustomRecords) map[uint64][]byte {
 
+	if !ExperimentalEndorsementActive() {
+		// Return nil if there are no records, to match wire encoding.
+		if len(originalRecords) == 0 {
+			return nil
+		}
+
+		return originalRecords.Copy()
+	}
+
 	return originalRecords.MergedCopy(map[uint64][]byte{
 		uint64(lnwire.ExperimentalEndorsementType): {
 			lnwire.ExperimentalUnendorsed,
 		}},
 	)
+}
+
+// ExperimentalEndorsementActive returns true if the experimental endorsement
+// window is still open.
+func ExperimentalEndorsementActive() bool {
+	return time.Now().Before(lnd.EndorsementExperimentEnd)
 }
 
 // LnrpcOutpointToStr returns a string representation of an lnrpc.OutPoint.

flokiorg/flnd

lntest/utils.go
@@ -7,7 +7,9 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/flokiorg/flnd"
 	"github.com/flokiorg/flnd/input"
 	"github.com/flokiorg/flnd/lnrpc"
 	"github.com/flokiorg/flnd/lntest/wait"
@@ -288,11 +290,26 @@ func CalcStaticFeeBuffer(c lnrpc.CommitmentType, numHTLCs int) chainutil.Amount 
 func CustomRecordsWithUnendorsed(
 	originalRecords lnwire.CustomRecords) map[uint64][]byte {
 
+	if !ExperimentalEndorsementActive() {
+		// Return nil if there are no records, to match wire encoding.
+		if len(originalRecords) == 0 {
+			return nil
+		}
+
+		return originalRecords.Copy()
+	}
+
 	return originalRecords.MergedCopy(map[uint64][]byte{
 		uint64(lnwire.ExperimentalEndorsementType): {
 			lnwire.ExperimentalUnendorsed,
 		}},
 	)
+}
+
+// ExperimentalEndorsementActive returns true if the experimental endorsement
+// window is still open.
+func ExperimentalEndorsementActive() bool {
+	return time.Now().Before(flnd.EndorsementExperimentEnd)
 }
 
 // LnrpcOutpointToStr returns a string representation of an lnrpc.OutPoint.