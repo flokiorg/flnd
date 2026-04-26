elnosh/lnd

server: remove accountability signal experiment period
In the previous iteration with endorsement
signaling, the recommendation was for the sender to
set it to 1 and that could have had privacy concerns
when first deploying given that the default was to
downgrade the signal to 0. In the latest proposal
the recommended default for both sending and
forwarding nodes is to set `accountable` to 0.
As a result, the dates have been removed given
that there are no privacy risks associated
with relaying the signal with zero values.

rpcserver.go
@@ -775,13 +775,7 @@ func (r *rpcServer) addDeps(ctx context.Context, s *server,
 			return nil
 		},
 		ShouldSetExpAccountability: func() bool {
-			if s.cfg.ProtocolOptions.NoExperimentalAccountability() {
-				return false
-			}
-
-			return clock.NewDefaultClock().Now().Before(
-				AccountabilityExperimentEnd,
-			)
+			return !s.cfg.ProtocolOptions.NoExperimentalAccountability()
 		},
 	}
 

server.go
@@ -138,12 +138,6 @@ var (
 	// TODO(roasbeef): add command line param to modify.
 	MaxFundingAmount = funding.MaxBtcFundingAmount
 
-	// AccountabilityExperimentEnd is the time after which nodes should stop
-	// propagating experimental accountable signals.
-	//
-	// Per blip04: January 1, 2026 12:00:00 AM UTC in unix seconds.
-	AccountabilityExperimentEnd = time.Unix(1767225600, 0)
-
 	// ErrGossiperBan is one of the errors that can be returned when we
 	// attempt to finalize a connection to a remote peer.
 	ErrGossiperBan = errors.New("gossiper has banned remote's key")
@@ -4421,13 +4415,7 @@ func (s *server) peerConnected(conn net.Conn, connReq *connmgr.ConnReq,
 		AuxTrafficShaper:       s.implCfg.TrafficShaper,
 		AuxChannelNegotiator:   s.implCfg.AuxChannelNegotiator,
 		ShouldFwdExpAccountability: func() bool {
-			if s.cfg.ProtocolOptions.NoExperimentalAccountability() {
-				return false
-			}
-
-			return clock.NewDefaultClock().Now().Before(
-				AccountabilityExperimentEnd,
-			)
+			return !s.cfg.ProtocolOptions.NoExperimentalAccountability()
 		},
 		NoDisconnectOnPongFailure: s.cfg.NoDisconnectOnPongFailure,
 	}

flokiorg/flnd

rpcserver.go
@@ -781,13 +781,7 @@ func (r *rpcServer) addDeps(ctx context.Context, s *server,
 			return nil
 		},
 		ShouldSetExpAccountability: func() bool {
-			if s.cfg.ProtocolOptions.NoExpAccountability() {
-				return false
-			}
-
-			return clock.NewDefaultClock().Now().Before(
-				AccountabilityExperimentEnd,
-			)
+			return !s.cfg.ProtocolOptions.NoExpAccountability()
 		},
 	}
 

server.go
@@ -140,12 +140,6 @@ var (
 	// TODO(roasbeef): add command line param to modify.
 	MaxFundingAmount = funding.MaxBtcFundingAmount
 
-	// AccountabilityExperimentEnd is the time after which nodes should stop
-	// propagating experimental accountable signals.
-	//
-	// Per blip04: January 1, 2026 12:00:00 AM UTC in unix seconds.
-	AccountabilityExperimentEnd = time.Unix(1767225600, 0)
-
 	// ErrGossiperBan is one of the errors that can be returned when we
 	// attempt to finalize a connection to a remote peer.
 	ErrGossiperBan = errors.New("gossiper has banned remote's key")
@@ -4428,13 +4422,7 @@ func (s *server) peerConnected(conn net.Conn, connReq *connmgr.ConnReq,
 		AuxTrafficShaper:       s.implCfg.TrafficShaper,
 		AuxChannelNegotiator:   s.implCfg.AuxChannelNegotiator,
 		ShouldFwdExpAccountability: func() bool {
-			if s.cfg.ProtocolOptions.NoExpAccountability() {
-				return false
-			}
-
-			return clock.NewDefaultClock().Now().Before(
-				AccountabilityExperimentEnd,
-			)
+			return !s.cfg.ProtocolOptions.NoExpAccountability()
 		},
 		NoDisconnectOnPongFailure: s.cfg.NoDisconnectOnPongFailure,
 	}