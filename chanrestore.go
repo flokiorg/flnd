ziggie1984/lnd

lnd: use chanstate Store for channel restore
Make chanDBRestorer persist restored channel shells through
chanstate.Store instead of the concrete ChannelStateDB. The restorer
still builds channeldb channel shell values, but only needs
RestoreChannelShells from the store.

chanrestore.go
@@ -11,6 +11,7 @@ import (
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/chanbackup"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/chanstate"
 	"github.com/lightningnetwork/lnd/contractcourt"
 	"github.com/lightningnetwork/lnd/keychain"
 	"github.com/lightningnetwork/lnd/lnwire"
@@ -35,7 +36,7 @@ const (
 // need the secret key chain in order obtain the prior shachain root so we can
 // verify the DLP protocol as initiated by the remote node.
 type chanDBRestorer struct {
-	db *channeldb.ChannelStateDB
+	db chanstate.Store
 
 	secretKeys keychain.SecretKeyRing
 

flokiorg/flnd

chanrestore.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/flokiorg/flnd/chanbackup"
 	"github.com/flokiorg/flnd/channeldb"
+	"github.com/flokiorg/flnd/chanstate"
 	"github.com/flokiorg/flnd/contractcourt"
 	"github.com/flokiorg/flnd/keychain"
 	"github.com/flokiorg/flnd/lnwire"
@@ -36,7 +37,7 @@ const (
 // need the secret key chain in order obtain the prior shachain root so we can
 // verify the DLP protocol as initiated by the remote node.
 type chanDBRestorer struct {
-	db *channeldb.ChannelStateDB
+	db chanstate.OpenChannelStore
 
 	secretKeys keychain.SecretKeyRing
 