ziggie1984/lnd

lnrpc: depend on chanstate Store
Replace concrete ChannelStateDB fields in the invoices and wallet RPC
configs with chanstate.Store, and update the subserver dependency
wiring to pass the interface. The affected RPC paths only need
channel-state store methods for hop hints and waiting-close channel
queries.

lnrpc/invoicesrpc/addinvoice.go
@@ -18,6 +18,7 @@ import (
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/wire"
 	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/chanstate"
 	"github.com/lightningnetwork/lnd/graph/db/models"
 	"github.com/lightningnetwork/lnd/invoices"
 	"github.com/lightningnetwork/lnd/lntypes"
@@ -72,7 +73,7 @@ type AddInvoiceConfig struct {
 
 	// ChanDB is a global boltdb instance which is needed to access the
 	// channel graph.
-	ChanDB *channeldb.ChannelStateDB
+	ChanDB chanstate.Store
 
 	// Graph gives the invoice server access to various graph related
 	// queries.

subrpcserver_config.go
@@ -11,7 +11,7 @@ import (
 	"github.com/lightningnetwork/lnd/aliasmgr"
 	"github.com/lightningnetwork/lnd/autopilot"
 	"github.com/lightningnetwork/lnd/chainreg"
-	"github.com/lightningnetwork/lnd/channeldb"
+	"github.com/lightningnetwork/lnd/chanstate"
 	"github.com/lightningnetwork/lnd/fn/v2"
 	graphdb "github.com/lightningnetwork/lnd/graph/db"
 	"github.com/lightningnetwork/lnd/htlcswitch"
@@ -115,7 +115,7 @@ func (s *subRPCServerConfigs) PopulateDependencies(cfg *Config,
 	routerBackend *routerrpc.RouterBackend,
 	nodeSigner *netann.NodeSigner,
 	graphDB *graphdb.ChannelGraph,
-	chanStateDB *channeldb.ChannelStateDB,
+	chanStateDB chanstate.Store,
 	sweeper *sweep.UtxoSweeper,
 	tower *watchtower.Standalone,
 	towerClientMgr *wtclient.Manager,

flokiorg/flnd

lnrpc/invoicesrpc/addinvoice.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	"github.com/flokiorg/flnd/channeldb"
+	"github.com/flokiorg/flnd/chanstate"
 	"github.com/flokiorg/flnd/graph/db/models"
 	"github.com/flokiorg/flnd/invoices"
 	"github.com/flokiorg/flnd/lntypes"
@@ -70,9 +71,8 @@ type AddInvoiceConfig struct {
 	// specified.
 	DefaultCLTVExpiry uint32
 
-	// ChanDB is a global boltdb instance which is needed to access the
-	// channel graph.
-	ChanDB *channeldb.ChannelStateDB
+	// ChanDB is used to access open channel state.
+	ChanDB chanstate.OpenChannelStore
 
 	// Graph gives the invoice server access to various graph related
 	// queries.

subrpcserver_config.go
@@ -9,7 +9,7 @@ import (
 	"github.com/flokiorg/flnd/aliasmgr"
 	"github.com/flokiorg/flnd/autopilot"
 	"github.com/flokiorg/flnd/chainreg"
-	"github.com/flokiorg/flnd/channeldb"
+	"github.com/flokiorg/flnd/chanstate"
 	"github.com/flokiorg/flnd/fn"
 	graphdb "github.com/flokiorg/flnd/graph/db"
 	"github.com/flokiorg/flnd/htlcswitch"
@@ -115,7 +115,7 @@ func (s *subRPCServerConfigs) PopulateDependencies(cfg *Config,
 	routerBackend *routerrpc.RouterBackend,
 	nodeSigner *netann.NodeSigner,
 	graphDB *graphdb.ChannelGraph,
-	chanStateDB *channeldb.ChannelStateDB,
+	chanStateDB chanstate.Store,
 	sweeper *sweep.UtxoSweeper,
 	tower *watchtower.Standalone,
 	towerClientMgr *wtclient.Manager,