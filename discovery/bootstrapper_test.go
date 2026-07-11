lightningnetwork/lnd

multi: drop tor v2 onion production, keep wire codec faithful
Tor stopped serving v2 onion services in October 2021; lnd should not
produce v2 addresses anymore, but it must still verify signatures on
and re-broadcast peer NodeAnnouncement messages that carry v2 entries.

Stop accepting v2 as configuration input (lncfg), strip the legacy
`--tor.v2` flag from the sample config, and remove the
`tor.OnionHostToFakeIP` helper. Operator entry points (`--externalip`,
`--listen`, `lncli connect`, `lncli wtclient towers add`) fail fast on
a v2 `.onion` string, so upgrading nodes must remove any v2 entry from
`lnd.conf` before lnd will start.

Filter persisted v2 state before use without rewriting on-disk records:
the self-announcement builder strips any v2 entry inherited from the
stored self-node; the watchtower client drops v2 entries from each
persisted tower's address list (skipping the tower entirely if no
non-v2 address remains); the autopilot connector, graph bootstrapper,
and static-channel backup restore paths skip v2 entries before
attempting outbound dials. Restrict the Tor controller's ADD_ONION
path to v3 keys, including the encrypted on-disk legacy-key fallback.

For inbound announcements, keep the wire codec wire-faithful:
`lnwire.WriteOnionAddr`, `graph/db.encodeOnionAddr`, and the matching
decoders round-trip v2 bytes so `DataToSign` reproduces the bytes the
remote peer signed, signature validation succeeds, and the announcement
is persisted to the graph DB and re-broadcast across restarts byte-for-
byte. RPC surfaces continue to expose the full address set so external
tools can independently reproduce and verify the signed bytes.

Add a netann regression test that signs a [v3, v2, ipv4] announcement,
round-trips it through Encode/Decode, verifies the signature, and
confirms the resulting models.Node preserves the v2 entry. Add a
graph bootstrapper test asserting v2 entries are skipped while v3 and
plain TCP entries on the same node still surface as bootstr

discovery/bootstrapper_test.go
@@ -1,0 +1,147 @@
+package discovery
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/lightningnetwork/lnd/autopilot"
+	"github.com/lightningnetwork/lnd/tor"
+	"github.com/stretchr/testify/require"
+)
+
+// stubNode is a minimal autopilot.Node implementation used to drive
+// ChannelGraphBootstrapper from a unit test.
+type stubNode struct {
+	pub   *btcec.PublicKey
+	addrs []net.Addr
+}
+
+func (n *stubNode) PubKey() [33]byte {
+	var out [33]byte
+	copy(out[:], n.pub.SerializeCompressed())
+	return out
+}
+
+func (n *stubNode) Addrs() []net.Addr { return n.addrs }
+
+// stubChannelGraph yields a fixed list of nodes from ForEachNode and is
+// otherwise unused by SampleNodeAddrs.
+type stubChannelGraph struct {
+	nodes []autopilot.Node
+}
+
+// ForEachNode invokes the callback for each node in the stub graph,
+// stopping early if the callback returns a non-nil error.
+func (s *stubChannelGraph) ForEachNode(ctx context.Context,
+	cb func(context.Context, autopilot.Node) error, _ func()) error {
+
+	for _, n := range s.nodes {
+		if err := cb(ctx, n); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// ForEachNodesChannels is a no-op stub; SampleNodeAddrs does not exercise
+// the channel iteration path.
+func (s *stubChannelGraph) ForEachNodesChannels(_ context.Context,
+	_ func(context.Context, autopilot.NodeID,
+		[]*autopilot.ChannelEdge) error, _ func()) error {
+
+	return nil
+}
+
+// TestGraphBootstrapperSkipsV2Onion ensures SampleNodeAddrs strips Tor v2
+// .onion entries from the returned candidate set so the connection manager
+// never attempts to dial an obsolete v2 hidden service surfaced through the
+// channel graph. A node whose remaining addresses include a v3 .onion or
+// plain TCP entry is still returned for those addresses, and a node whose
+// only address is v2 contributes nothing.
+func TestGraphBootstrapperSkipsV2Onion(t *testing.T) {
+	t.Parallel()
+
+	v2 := &tor.OnionAddr{
+		OnionService: "3g2upl4pq6kufc4m.onion",
+		Port:         9735,
+	}
+	v3 := &tor.OnionAddr{
+		OnionService: "4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnz" +
+			"jpbi7utijcltosqemad.onion",
+		Port: 9735,
+	}
+	tcp := &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9735}
+
+	mkPub := func(t *testing.T) *btcec.PublicKey {
+		t.Helper()
+		priv, err := btcec.NewPrivateKey()
+		require.NoError(t, err)
+
+		return priv.PubKey()
+	}
+
+	mixedPub := mkPub(t)
+	v2OnlyPub := mkPub(t)
+
+	graph := &stubChannelGraph{
+		nodes: []autopilot.Node{
+			&stubNode{
+				pub:   mixedPub,
+				addrs: []net.Addr{v2, v3, tcp},
+			},
+			&stubNode{
+				pub:   v2OnlyPub,
+				addrs: []net.Addr{v2},
+			},
+		},
+	}
+
+	// Use deterministic sampling so the hash accumulator never skips a
+	// node — every eligible address must surface on each call.
+	bs, err := NewGraphBootstrapper(graph, true)
+	require.NoError(t, err)
+
+	// Ask for more addresses than the graph holds so the bootstrapper
+	// drains it in a single pass.
+	got, err := bs.SampleNodeAddrs(
+		t.Context(), 10, map[autopilot.NodeID]struct{}{},
+	)
+	require.NoError(t, err)
+
+	// Collect (pubkey, address) pairs we expect — the v2-only node must
+	// not contribute any NetAddress, and the mixed node contributes only
+	// v3 and tcp.
+	type seenAddr struct {
+		pub  [33]byte
+		addr string
+	}
+	seen := make(map[seenAddr]struct{}, len(got))
+	for _, na := range got {
+		var p [33]byte
+		copy(p[:], na.IdentityKey.SerializeCompressed())
+		seen[seenAddr{pub: p, addr: na.Address.String()}] = struct{}{}
+	}
+
+	var mixedKey, v2OnlyKey [33]byte
+	copy(mixedKey[:], mixedPub.SerializeCompressed())
+	copy(v2OnlyKey[:], v2OnlyPub.SerializeCompressed())
+
+	// Mixed node yields v3 and tcp entries.
+	require.Contains(t, seen, seenAddr{
+		pub: mixedKey, addr: v3.String(),
+	})
+	require.Contains(t, seen, seenAddr{
+		pub: mixedKey, addr: tcp.String(),
+	})
+
+	// No v2 entry surfaces, for either node.
+	for s := range seen {
+		require.NotEqual(t, v2.String(), s.addr,
+			"v2 onion %v leaked into candidate set", s.addr)
+		require.NotEqual(t, v2OnlyKey, s.pub,
+			"v2-only node %x should contribute nothing", s.pub)
+	}
+}

netann/node_announcement_test.go
@@ -1,0 +1,90 @@
+package netann_test
+
+import (
+	"bytes"
+	"net"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcec/v2/ecdsa"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/lightningnetwork/lnd/graph/db/models"
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/netann"
+	"github.com/lightningnetwork/lnd/tor"
+	"github.com/stretchr/testify/require"
+)
+
+// TestNodeAnnSignatureWithLegacyV2OnionAddr signs a [v3, v2, ipv4]
+// announcement, round-trips it through the wire codec, and verifies that
+// the signature still validates. If Decode or NodeFromWireAnnouncement ever
+// starts filtering v2, DataToSign no longer reproduces the signed bytes and
+// this test catches the regression.
+func TestNodeAnnSignatureWithLegacyV2OnionAddr(t *testing.T) {
+	t.Parallel()
+
+	privKey, err := btcec.NewPrivateKey()
+	require.NoError(t, err)
+
+	var nodeID [33]byte
+	copy(nodeID[:], privKey.PubKey().SerializeCompressed())
+
+	v3 := &tor.OnionAddr{
+		OnionService: "abcdefghijabcdefghijabcdefghij" +
+			"abcdefghijabcdefghij234567.onion",
+		Port: 9735,
+	}
+	v2 := &tor.OnionAddr{
+		OnionService: "abcdefghijklmnop.onion",
+		Port:         9735,
+	}
+	tcp := &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 9735}
+	addrs := []net.Addr{v3, v2, tcp}
+
+	ann := &lnwire.NodeAnnouncement1{
+		Features:  lnwire.NewRawFeatureVector(),
+		Timestamp: 1700000000,
+		NodeID:    nodeID,
+		Addresses: addrs,
+	}
+	copy(ann.Alias[:], "regression-test")
+
+	dataToSign, err := ann.DataToSign()
+	require.NoError(t, err)
+
+	rawSig := ecdsa.Sign(privKey, chainhash.DoubleHashB(dataToSign))
+	ann.Signature, err = lnwire.NewSigFromSignature(rawSig)
+	require.NoError(t, err)
+
+	var buf bytes.Buffer
+	require.NoError(t, ann.Encode(&buf, 0))
+
+	var decoded lnwire.NodeAnnouncement1
+	require.NoError(t, decoded.Decode(&buf, 0))
+
+	// All three addresses must survive Decode for DataToSign to
+	// reproduce the signed bytes.
+	require.Len(t, decoded.Addresses, 3)
+	require.NoError(t, netann.ValidateNodeAnnSignature(&decoded))
+
+	node := models.NodeFromWireAnnouncement(&decoded)
+	require.Len(t, node.Addresses, 3)
+
+	var sawV2, sawV3, sawTCP bool
+	for _, addr := range node.Addresses {
+		switch a := addr.(type) {
+		case *tor.OnionAddr:
+			switch len(a.OnionService) {
+			case tor.V2Len:
+				sawV2 = true
+			case tor.V3Len:
+				sawV3 = true
+			}
+		case *net.TCPAddr:
+			sawTCP = true
+		}
+	}
+	require.True(t, sawV2, "v2 onion address must be preserved on the Node")
+	require.True(t, sawV3, "v3 onion address must be preserved on the Node")
+	require.True(t, sawTCP, "ipv4 address must be preserved on the Node")
+}

server_test.go
@@ -1,9 +1,11 @@
 package lnd
 
 import (
+	"net"
 	"testing"
 	"time"
 
+	"github.com/lightningnetwork/lnd/tor"
 	"github.com/stretchr/testify/require"
 )
 
@@ -138,4 +140,101 @@ func TestNodeAnnouncementTimestampComparison(t *testing.T) {
 			)
 		})
 	}
+}
+
+// TestParseAddrRejectsTorV2 ensures that parseAddr rejects v2 .onion hosts at
+// the operator-input boundary. This is the path used by lncli connect (via
+// rpcserver.ConnectPeer) and the --addpeer config option, mirroring the
+// equivalent gate in lncfg.ParseAddressString.
+func TestParseAddrRejectsTorV2(t *testing.T) {
+	t.Parallel()
+
+	const (
+		v2Host = "3g2upl4pq6kufc4m.onion"
+		v3Host = "4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnzjpb" +
+			"i7utijcltosqemad.onion"
+	)
+
+	netCfg := &tor.ClearNet{}
+
+	tests := []struct {
+		name      string
+		address   string
+		expectErr bool
+	}{
+		{
+			name:      "v2 without port is rejected",
+			address:   v2Host,
+			expectErr: true,
+		},
+		{
+			name:      "v2 with port is rejected",
+			address:   v2Host + ":9735",
+			expectErr: true,
+		},
+		{
+			name:      "v3 without port is accepted",
+			address:   v3Host,
+			expectErr: false,
+		},
+		{
+			name:      "v3 with port is accepted",
+			address:   v3Host + ":9735",
+			expectErr: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			addr, err := parseAddr(tc.address, netCfg)
+			if tc.expectErr {
+				require.Error(t, err)
+				require.Contains(
+					t, err.Error(), "tor v2 onion",
+				)
+				require.Nil(t, addr)
+
+				return
+			}
+
+			require.NoError(t, err)
+			onionAddr, ok := addr.(*tor.OnionAddr)
+			require.True(t, ok)
+			require.Equal(t, v3Host, onionAddr.OnionService)
+		})
+	}
+}
+
+// TestWithoutV2Onion ensures that Tor v2 onion addresses are dropped from
+// reconnect/dial consumption paths (startup persistent reconnect, live
+// topology updates, fetchNodeAdvertisedAddrs) while non-onion and v3 onion
+// addresses pass through unchanged. Storage and gossip re-broadcast remain
+// byte-faithful elsewhere.
+func TestWithoutV2Onion(t *testing.T) {
+	t.Parallel()
+
+	v2 := &tor.OnionAddr{
+		OnionService: "3g2upl4pq6kufc4m.onion",
+		Port:         9735,
+	}
+	v3 := &tor.OnionAddr{
+		OnionService: "4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnz" +
+			"jpbi7utijcltosqemad.onion",
+		Port: 9735,
+	}
+	tcp := &net.TCPAddr{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: 9735,
+	}
+
+	require.True(t, isV2OnionAddr(v2))
+	require.False(t, isV2OnionAddr(v3))
+	require.False(t, isV2OnionAddr(tcp))
+
+	filtered := withoutV2Onion([]net.Addr{v2, v3, tcp, v2})
+	require.Equal(t, []net.Addr{v3, tcp}, filtered)
+
+	// An all-v2 input filters to an empty slice; callers such as
+	// fetchNodeAdvertisedAddrs treat this as "no advertised address".
+	require.Empty(t, withoutV2Onion([]net.Addr{v2, v2}))
 }

watchtower/wtclient/interface_test.go
@@ -1,0 +1,57 @@
+package wtclient
+
+import (
+	"net"
+	"testing"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/lightningnetwork/lnd/tor"
+	"github.com/lightningnetwork/lnd/watchtower/wtdb"
+	"github.com/stretchr/testify/require"
+)
+
+// TestNewTowerFromDBTowerFiltersV2Onion asserts that NewTowerFromDBTower drops
+// any persisted Tor v2 .onion entries before constructing the address
+// iterator, that mixed lists still surface the remaining v3/tcp addresses, and
+// that a tower whose addresses are exclusively v2 surfaces
+// ErrTowerOnlyV2Onion so the caller can skip it without touching the DB.
+func TestNewTowerFromDBTowerFiltersV2Onion(t *testing.T) {
+	t.Parallel()
+
+	priv, err := btcec.NewPrivateKey()
+	require.NoError(t, err)
+
+	v2 := &tor.OnionAddr{
+		OnionService: "3g2upl4pq6kufc4m.onion",
+		Port:         9911,
+	}
+	v3 := &tor.OnionAddr{
+		OnionService: "4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnz" +
+			"jpbi7utijcltosqemad.onion",
+		Port: 9911,
+	}
+	tcp := &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9911}
+
+	t.Run("mixed addresses keep v3/tcp", func(t *testing.T) {
+		t.Parallel()
+
+		tower, err := NewTowerFromDBTower(&wtdb.Tower{
+			ID:          7,
+			IdentityKey: priv.PubKey(),
+			Addresses:   []net.Addr{v2, v3, tcp, v2},
+		})
+		require.NoError(t, err)
+		require.Equal(t, []net.Addr{v3, tcp}, tower.Addresses.GetAll())
+	})
+
+	t.Run("only v2 addresses returns sentinel", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := NewTowerFromDBTower(&wtdb.Tower{
+			ID:          7,
+			IdentityKey: priv.PubKey(),
+			Addresses:   []net.Addr{v2, v2},
+		})
+		require.ErrorIs(t, err, ErrTowerOnlyV2Onion)
+	})
+}

flokiorg/flnd

discovery/bootstrapper_test.go
@@ -1,0 +1,147 @@
+package discovery
+
+import (
+	"context"
+	"net"
+	"testing"
+
+	"github.com/flokiorg/flnd/autopilot"
+	"github.com/flokiorg/flnd/tor"
+	"github.com/flokiorg/go-flokicoin/crypto"
+	"github.com/stretchr/testify/require"
+)
+
+// stubNode is a minimal autopilot.Node implementation used to drive
+// ChannelGraphBootstrapper from a unit test.
+type stubNode struct {
+	pub   *crypto.PublicKey
+	addrs []net.Addr
+}
+
+func (n *stubNode) PubKey() [33]byte {
+	var out [33]byte
+	copy(out[:], n.pub.SerializeCompressed())
+	return out
+}
+
+func (n *stubNode) Addrs() []net.Addr { return n.addrs }
+
+// stubChannelGraph yields a fixed list of nodes from ForEachNode and is
+// otherwise unused by SampleNodeAddrs.
+type stubChannelGraph struct {
+	nodes []autopilot.Node
+}
+
+// ForEachNode invokes the callback for each node in the stub graph,
+// stopping early if the callback returns a non-nil error.
+func (s *stubChannelGraph) ForEachNode(ctx context.Context,
+	cb func(context.Context, autopilot.Node) error, _ func()) error {
+
+	for _, n := range s.nodes {
+		if err := cb(ctx, n); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// ForEachNodesChannels is a no-op stub; SampleNodeAddrs does not exercise
+// the channel iteration path.
+func (s *stubChannelGraph) ForEachNodesChannels(_ context.Context,
+	_ func(context.Context, autopilot.NodeID,
+		[]*autopilot.ChannelEdge) error, _ func()) error {
+
+	return nil
+}
+
+// TestGraphBootstrapperSkipsV2Onion ensures SampleNodeAddrs strips Tor v2
+// .onion entries from the returned candidate set so the connection manager
+// never attempts to dial an obsolete v2 hidden service surfaced through the
+// channel graph. A node whose remaining addresses include a v3 .onion or
+// plain TCP entry is still returned for those addresses, and a node whose
+// only address is v2 contributes nothing.
+func TestGraphBootstrapperSkipsV2Onion(t *testing.T) {
+	t.Parallel()
+
+	v2 := &tor.OnionAddr{
+		OnionService: "3g2upl4pq6kufc4m.onion",
+		Port:         9735,
+	}
+	v3 := &tor.OnionAddr{
+		OnionService: "4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnz" +
+			"jpbi7utijcltosqemad.onion",
+		Port: 9735,
+	}
+	tcp := &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9735}
+
+	mkPub := func(t *testing.T) *crypto.PublicKey {
+		t.Helper()
+		priv, err := crypto.NewPrivateKey()
+		require.NoError(t, err)
+
+		return priv.PubKey()
+	}
+
+	mixedPub := mkPub(t)
+	v2OnlyPub := mkPub(t)
+
+	graph := &stubChannelGraph{
+		nodes: []autopilot.Node{
+			&stubNode{
+				pub:   mixedPub,
+				addrs: []net.Addr{v2, v3, tcp},
+			},
+			&stubNode{
+				pub:   v2OnlyPub,
+				addrs: []net.Addr{v2},
+			},
+		},
+	}
+
+	// Use deterministic sampling so the hash accumulator never skips a
+	// node — every eligible address must surface on each call.
+	bs, err := NewGraphBootstrapper(graph, true)
+	require.NoError(t, err)
+
+	// Ask for more addresses than the graph holds so the bootstrapper
+	// drains it in a single pass.
+	got, err := bs.SampleNodeAddrs(
+		t.Context(), 10, map[autopilot.NodeID]struct{}{},
+	)
+	require.NoError(t, err)
+
+	// Collect (pubkey, address) pairs we expect — the v2-only node must
+	// not contribute any NetAddress, and the mixed node contributes only
+	// v3 and tcp.
+	type seenAddr struct {
+		pub  [33]byte
+		addr string
+	}
+	seen := make(map[seenAddr]struct{}, len(got))
+	for _, na := range got {
+		var p [33]byte
+		copy(p[:], na.IdentityKey.SerializeCompressed())
+		seen[seenAddr{pub: p, addr: na.Address.String()}] = struct{}{}
+	}
+
+	var mixedKey, v2OnlyKey [33]byte
+	copy(mixedKey[:], mixedPub.SerializeCompressed())
+	copy(v2OnlyKey[:], v2OnlyPub.SerializeCompressed())
+
+	// Mixed node yields v3 and tcp entries.
+	require.Contains(t, seen, seenAddr{
+		pub: mixedKey, addr: v3.String(),
+	})
+	require.Contains(t, seen, seenAddr{
+		pub: mixedKey, addr: tcp.String(),
+	})
+
+	// No v2 entry surfaces, for either node.
+	for s := range seen {
+		require.NotEqual(t, v2.String(), s.addr,
+			"v2 onion %v leaked into candidate set", s.addr)
+		require.NotEqual(t, v2OnlyKey, s.pub,
+			"v2-only node %x should contribute nothing", s.pub)
+	}
+}

netann/node_announcement_test.go
@@ -1,0 +1,90 @@
+package netann_test
+
+import (
+	"bytes"
+	"net"
+	"testing"
+
+	"github.com/flokiorg/flnd/graph/db/models"
+	"github.com/flokiorg/flnd/lnwire"
+	"github.com/flokiorg/flnd/netann"
+	"github.com/flokiorg/flnd/tor"
+	"github.com/flokiorg/go-flokicoin/chaincfg/chainhash"
+	"github.com/flokiorg/go-flokicoin/crypto"
+	"github.com/flokiorg/go-flokicoin/crypto/ecdsa"
+	"github.com/stretchr/testify/require"
+)
+
+// TestNodeAnnSignatureWithLegacyV2OnionAddr signs a [v3, v2, ipv4]
+// announcement, round-trips it through the wire codec, and verifies that
+// the signature still validates. If Decode or NodeFromWireAnnouncement ever
+// starts filtering v2, DataToSign no longer reproduces the signed bytes and
+// this test catches the regression.
+func TestNodeAnnSignatureWithLegacyV2OnionAddr(t *testing.T) {
+	t.Parallel()
+
+	privKey, err := crypto.NewPrivateKey()
+	require.NoError(t, err)
+
+	var nodeID [33]byte
+	copy(nodeID[:], privKey.PubKey().SerializeCompressed())
+
+	v3 := &tor.OnionAddr{
+		OnionService: "abcdefghijabcdefghijabcdefghij" +
+			"abcdefghijabcdefghij234567.onion",
+		Port: 9735,
+	}
+	v2 := &tor.OnionAddr{
+		OnionService: "abcdefghijklmnop.onion",
+		Port:         9735,
+	}
+	tcp := &net.TCPAddr{IP: net.IPv4(127, 0, 0, 1), Port: 9735}
+	addrs := []net.Addr{v3, v2, tcp}
+
+	ann := &lnwire.NodeAnnouncement1{
+		Features:  lnwire.NewRawFeatureVector(),
+		Timestamp: 1700000000,
+		NodeID:    nodeID,
+		Addresses: addrs,
+	}
+	copy(ann.Alias[:], "regression-test")
+
+	dataToSign, err := ann.DataToSign()
+	require.NoError(t, err)
+
+	rawSig := ecdsa.Sign(privKey, chainhash.DoubleHashB(dataToSign))
+	ann.Signature, err = lnwire.NewSigFromSignature(rawSig)
+	require.NoError(t, err)
+
+	var buf bytes.Buffer
+	require.NoError(t, ann.Encode(&buf, 0))
+
+	var decoded lnwire.NodeAnnouncement1
+	require.NoError(t, decoded.Decode(&buf, 0))
+
+	// All three addresses must survive Decode for DataToSign to
+	// reproduce the signed bytes.
+	require.Len(t, decoded.Addresses, 3)
+	require.NoError(t, netann.ValidateNodeAnnSignature(&decoded))
+
+	node := models.NodeFromWireAnnouncement(&decoded)
+	require.Len(t, node.Addresses, 3)
+
+	var sawV2, sawV3, sawTCP bool
+	for _, addr := range node.Addresses {
+		switch a := addr.(type) {
+		case *tor.OnionAddr:
+			switch len(a.OnionService) {
+			case tor.V2Len:
+				sawV2 = true
+			case tor.V3Len:
+				sawV3 = true
+			}
+		case *net.TCPAddr:
+			sawTCP = true
+		}
+	}
+	require.True(t, sawV2, "v2 onion address must be preserved on the Node")
+	require.True(t, sawV3, "v3 onion address must be preserved on the Node")
+	require.True(t, sawTCP, "ipv4 address must be preserved on the Node")
+}

server_test.go
@@ -1,9 +1,11 @@
 package flnd
 
 import (
+	"net"
 	"testing"
 	"time"
 
+	"github.com/flokiorg/flnd/tor"
 	"github.com/stretchr/testify/require"
 )
 
@@ -138,4 +140,101 @@ func TestNodeAnnouncementTimestampComparison(t *testing.T) {
 			)
 		})
 	}
+}
+
+// TestParseAddrRejectsTorV2 ensures that parseAddr rejects v2 .onion hosts at
+// the operator-input boundary. This is the path used by lncli connect (via
+// rpcserver.ConnectPeer) and the --addpeer config option, mirroring the
+// equivalent gate in lncfg.ParseAddressString.
+func TestParseAddrRejectsTorV2(t *testing.T) {
+	t.Parallel()
+
+	const (
+		v2Host = "3g2upl4pq6kufc4m.onion"
+		v3Host = "4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnzjpb" +
+			"i7utijcltosqemad.onion"
+	)
+
+	netCfg := &tor.ClearNet{}
+
+	tests := []struct {
+		name      string
+		address   string
+		expectErr bool
+	}{
+		{
+			name:      "v2 without port is rejected",
+			address:   v2Host,
+			expectErr: true,
+		},
+		{
+			name:      "v2 with port is rejected",
+			address:   v2Host + ":9735",
+			expectErr: true,
+		},
+		{
+			name:      "v3 without port is accepted",
+			address:   v3Host,
+			expectErr: false,
+		},
+		{
+			name:      "v3 with port is accepted",
+			address:   v3Host + ":9735",
+			expectErr: false,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			addr, err := parseAddr(tc.address, netCfg)
+			if tc.expectErr {
+				require.Error(t, err)
+				require.Contains(
+					t, err.Error(), "tor v2 onion",
+				)
+				require.Nil(t, addr)
+
+				return
+			}
+
+			require.NoError(t, err)
+			onionAddr, ok := addr.(*tor.OnionAddr)
+			require.True(t, ok)
+			require.Equal(t, v3Host, onionAddr.OnionService)
+		})
+	}
+}
+
+// TestWithoutV2Onion ensures that Tor v2 onion addresses are dropped from
+// reconnect/dial consumption paths (startup persistent reconnect, live
+// topology updates, fetchNodeAdvertisedAddrs) while non-onion and v3 onion
+// addresses pass through unchanged. Storage and gossip re-broadcast remain
+// byte-faithful elsewhere.
+func TestWithoutV2Onion(t *testing.T) {
+	t.Parallel()
+
+	v2 := &tor.OnionAddr{
+		OnionService: "3g2upl4pq6kufc4m.onion",
+		Port:         9735,
+	}
+	v3 := &tor.OnionAddr{
+		OnionService: "4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnz" +
+			"jpbi7utijcltosqemad.onion",
+		Port: 9735,
+	}
+	tcp := &net.TCPAddr{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: 9735,
+	}
+
+	require.True(t, isV2OnionAddr(v2))
+	require.False(t, isV2OnionAddr(v3))
+	require.False(t, isV2OnionAddr(tcp))
+
+	filtered := withoutV2Onion([]net.Addr{v2, v3, tcp, v2})
+	require.Equal(t, []net.Addr{v3, tcp}, filtered)
+
+	// An all-v2 input filters to an empty slice; callers such as
+	// fetchNodeAdvertisedAddrs treat this as "no advertised address".
+	require.Empty(t, withoutV2Onion([]net.Addr{v2, v2}))
 }

watchtower/wtclient/interface_test.go
@@ -1,0 +1,57 @@
+package wtclient
+
+import (
+	"net"
+	"testing"
+
+	"github.com/flokiorg/flnd/tor"
+	"github.com/flokiorg/flnd/watchtower/wtdb"
+	"github.com/flokiorg/go-flokicoin/crypto"
+	"github.com/stretchr/testify/require"
+)
+
+// TestNewTowerFromDBTowerFiltersV2Onion asserts that NewTowerFromDBTower drops
+// any persisted Tor v2 .onion entries before constructing the address
+// iterator, that mixed lists still surface the remaining v3/tcp addresses, and
+// that a tower whose addresses are exclusively v2 surfaces
+// ErrTowerOnlyV2Onion so the caller can skip it without touching the DB.
+func TestNewTowerFromDBTowerFiltersV2Onion(t *testing.T) {
+	t.Parallel()
+
+	priv, err := crypto.NewPrivateKey()
+	require.NoError(t, err)
+
+	v2 := &tor.OnionAddr{
+		OnionService: "3g2upl4pq6kufc4m.onion",
+		Port:         9911,
+	}
+	v3 := &tor.OnionAddr{
+		OnionService: "4acth47i6kxnvkewtm6q7ib2s3ufpo5sqbsnz" +
+			"jpbi7utijcltosqemad.onion",
+		Port: 9911,
+	}
+	tcp := &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 9911}
+
+	t.Run("mixed addresses keep v3/tcp", func(t *testing.T) {
+		t.Parallel()
+
+		tower, err := NewTowerFromDBTower(&wtdb.Tower{
+			ID:          7,
+			IdentityKey: priv.PubKey(),
+			Addresses:   []net.Addr{v2, v3, tcp, v2},
+		})
+		require.NoError(t, err)
+		require.Equal(t, []net.Addr{v3, tcp}, tower.Addresses.GetAll())
+	})
+
+	t.Run("only v2 addresses returns sentinel", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := NewTowerFromDBTower(&wtdb.Tower{
+			ID:          7,
+			IdentityKey: priv.PubKey(),
+			Addresses:   []net.Addr{v2, v2},
+		})
+		require.ErrorIs(t, err, ErrTowerOnlyV2Onion)
+	})
+}