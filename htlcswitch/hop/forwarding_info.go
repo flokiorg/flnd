lightningnetwork/lnd

htlcswitch+invoices: align final-hop CLTV handling
Apply the same supported CLTV delta range to final-hop HTLC handling that is
already used for forwarding.

Use a shared helper for the exit-hop link path so final-hop amount and CLTV
checks remain consistent across invoice creation and HTLC handling.

htlcswitch/hop/forwarding_info.go
@@ -34,3 +34,52 @@ type ForwardingInfo struct {
 	// correct context.
 	PathID *chainhash.Hash
 }
+
+// FinalHtlcValidationResult describes the result of checking a final-hop
+// HTLC against the onion payload and supported final-hop CLTV range.
+type FinalHtlcValidationResult uint8
+
+const (
+	// FinalHtlcValid indicates that the HTLC matches the final-hop payload
+	// and supported final-hop CLTV range.
+	FinalHtlcValid FinalHtlcValidationResult = iota
+
+	// FinalHtlcInvalidAmount indicates that the HTLC amount is below the
+	// final amount requested by the onion payload.
+	FinalHtlcInvalidAmount
+
+	// FinalHtlcInvalidCltv indicates that the HTLC expiry is below the
+	// final CLTV requested by the onion payload.
+	FinalHtlcInvalidCltv
+
+	// FinalHtlcExpiryTooFar indicates that the HTLC expiry is outside the
+	// supported final-hop CLTV range.
+	FinalHtlcExpiryTooFar
+)
+
+// ValidateFinalHtlc checks final-hop HTLC amount and CLTV details before
+// invoice resolution.
+func ValidateFinalHtlc(amt lnwire.MilliSatoshi, expiry, heightNow,
+	maxFinalCltvDelta uint32, fwdInfo ForwardingInfo,
+	validateAmount bool) FinalHtlcValidationResult {
+
+	switch {
+	// The HTLC amount is below the final amount requested by the
+	// onion payload.
+	case validateAmount && amt < fwdInfo.AmountToForward:
+		return FinalHtlcInvalidAmount
+
+	// The HTLC expiry is below the final CLTV requested by the onion
+	// payload.
+	case expiry < fwdInfo.OutgoingCTLV:
+		return FinalHtlcInvalidCltv
+
+	// The HTLC expiry is outside the supported final-hop CLTV range.
+	case expiry > heightNow && expiry-heightNow > maxFinalCltvDelta:
+
+		return FinalHtlcExpiryTooFar
+
+	default:
+		return FinalHtlcValid
+	}
+}

htlcswitch/hop/forwarding_info_test.go
@@ -1,0 +1,137 @@
+package hop
+
+import (
+	"testing"
+
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/stretchr/testify/require"
+)
+
+// TestValidateFinalHtlc exercises the final-hop HTLC validation helper.
+func TestValidateFinalHtlc(t *testing.T) {
+	t.Parallel()
+
+	const (
+		amount       = lnwire.MilliSatoshi(1000)
+		expiry       = uint32(150)
+		height       = uint32(100)
+		maxCltvDelta = uint32(50)
+	)
+
+	fwdInfo := ForwardingInfo{
+		AmountToForward: amount,
+		OutgoingCTLV:    expiry,
+		NextHop:         Exit,
+	}
+
+	testCases := []struct {
+		name           string
+		amount         lnwire.MilliSatoshi
+		expiry         uint32
+		height         uint32
+		maxCltvDelta   uint32
+		fwdInfo        ForwardingInfo
+		validateAmount bool
+		expected       FinalHtlcValidationResult
+	}{{
+		name:           "valid",
+		amount:         amount,
+		expiry:         expiry,
+		height:         height + 1,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcValid,
+	}, {
+		name:           "amount too low",
+		amount:         amount - 1,
+		expiry:         expiry,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcInvalidAmount,
+	}, {
+		name:           "amount check disabled",
+		amount:         amount - 1,
+		expiry:         expiry,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: false,
+		expected:       FinalHtlcValid,
+	}, {
+		name:           "final cltv too low",
+		amount:         amount,
+		expiry:         expiry - 1,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcInvalidCltv,
+	}, {
+		name:           "expiry too far",
+		amount:         amount,
+		expiry:         expiry + 1,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcExpiryTooFar,
+	}, {
+		name:           "expiry at maximum",
+		amount:         amount,
+		expiry:         expiry,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcValid,
+	}, {
+		name:           "height above expiry",
+		amount:         amount,
+		expiry:         expiry,
+		height:         expiry + 1,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcValid,
+	}, {
+		name:           "amount failure takes precedence",
+		amount:         amount - 1,
+		expiry:         expiry - 1,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcInvalidAmount,
+	}, {
+		name: "cltv failure takes precedence over " +
+			"expiry too far",
+		amount:       amount,
+		expiry:       expiry + maxCltvDelta + 1,
+		height:       height,
+		maxCltvDelta: maxCltvDelta,
+		fwdInfo: ForwardingInfo{
+			AmountToForward: amount,
+			OutgoingCTLV:    expiry + maxCltvDelta + 2,
+			NextHop:         Exit,
+		},
+		validateAmount: true,
+		expected:       FinalHtlcInvalidCltv,
+	}}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			result := ValidateFinalHtlc(
+				testCase.amount, testCase.expiry,
+				testCase.height, testCase.maxCltvDelta,
+				testCase.fwdInfo, testCase.validateAmount,
+			)
+
+			require.Equal(t, testCase.expected, result)
+		})
+	}
+}

htlcswitch/link_test.go
@@ -6321,14 +6321,48 @@ func TestCheckHtlcForward(t *testing.T) {
 
 	})
 
-	t.Run("cltv expiry too far in the future", func(t *testing.T) {
-		// Check that expiry isn't too far in the future.
+	t.Run("cltv expiry outside supported range", func(t *testing.T) {
+		// Check that expiry stays within the supported range.
 		result := link.CheckHtlcForward(
 			hash, 1500, 1000, 10200, 10100, models.InboundFee{}, 0,
 			lnwire.ShortChannelID{}, nil,
 		)
+		_, ok := result.WireMessage().(*lnwire.FailExpiryTooFar)
+		if !ok {
+			t.Fatalf("expected FailExpiryTooFar failure code")
+		}
+	})
+
+	t.Run("incoming cltv delta outside range", func(t *testing.T) {
+		result := link.CheckHtlcForward(
+			hash, 1500, 1000, 150+DefaultMaxOutgoingCltvExpiry+1,
+			150, models.InboundFee{}, 0, lnwire.ShortChannelID{},
+			nil,
+		)
 		if _, ok := result.WireMessage().(*lnwire.FailExpiryTooFar); !ok {
 			t.Fatalf("expected FailExpiryTooFar failure code")
+		}
+	})
+
+	t.Run("incoming cltv delta at maximum", func(t *testing.T) {
+		result := link.CheckHtlcForward(
+			hash, 1500, 1000, 150+DefaultMaxOutgoingCltvExpiry,
+			150, models.InboundFee{}, 0, lnwire.ShortChannelID{},
+			nil,
+		)
+		require.Nil(t, result)
+	})
+
+	t.Run("incoming cltv below outgoing cltv", func(t *testing.T) {
+		result := link.CheckHtlcForward(
+			hash, 1500, 1000, 190, 200, models.InboundFee{}, 0,
+			lnwire.ShortChannelID{}, nil,
+		)
+		_, ok := result.WireMessage().(*lnwire.FailIncorrectCltvExpiry)
+		if !ok {
+			t.Fatalf(
+				"expected FailIncorrectCltvExpiry failure code",
+			)
 		}
 	})
 
@@ -6661,6 +6695,121 @@ func TestChannelLinkHoldInvoiceRestart(t *testing.T) {
 		t.Fatalf("did not expect message %T", msg)
 	default:
 	}
+}
+
+// TestChannelLinkExitHopExpiryTooFar asserts that an exit hop fails an
+// incoming HTLC if its expiry is outside the supported range.
+func TestChannelLinkExitHopExpiryTooFar(t *testing.T) {
+	t.Parallel()
+
+	const chanAmt = btcutil.SatoshiPerBitcoin * 5
+	harness, err := newSingleLinkTestHarness(t, chanAmt, 0)
+	require.NoError(t, err, "unable to create link")
+
+	if err := harness.start(); err != nil {
+		t.Fatalf("unable to start test harness: %v", err)
+	}
+	t.Cleanup(harness.aliceLink.Stop)
+
+	coreLink, ok := harness.aliceLink.(*channelLink)
+	require.True(t, ok)
+
+	registry, ok := coreLink.cfg.Registry.(*mockInvoiceRegistry)
+	require.True(t, ok)
+
+	alicePeer, ok := coreLink.cfg.Peer.(*mockPeer)
+	require.True(t, ok)
+	aliceMsgs := alicePeer.sentMsgs
+
+	registry.settleChan = make(chan lntypes.Hash)
+
+	htlc, invoice := generateHtlcAndInvoice(t, 0)
+	htlc.Expiry = testStartingHeight +
+		invpkg.MaxFinalCltvDelta + 1
+
+	err = registry.AddInvoice(t.Context(), *invoice, htlc.PaymentHash)
+	require.NoError(t, err, "unable to add invoice to registry")
+
+	ctx := linkTestContext{
+		t:           t,
+		aliceSwitch: harness.aliceSwitch,
+		aliceLink:   harness.aliceLink,
+		aliceMsgs:   aliceMsgs,
+		bobChannel:  harness.bobChannel,
+	}
+
+	ctx.sendHtlcBobToAlice(htlc)
+	ctx.sendCommitSigBobToAlice(1)
+	ctx.receiveRevAndAckAliceToBob()
+	ctx.receiveCommitSigAliceToBob(1)
+	ctx.sendRevAndAckBobToAlice()
+	ctx.receiveFailAliceToBobWithCode(
+		lnwire.CodeIncorrectOrUnknownPaymentDetails,
+	)
+	ctx.receiveCommitSigAliceToBob(0)
+
+	select {
+	case <-registry.settleChan:
+		t.Fatal("exit hop notification received")
+	case <-time.After(time.Second):
+	}
+}
+
+// TestChannelLinkExitHopExpiryAtMaximum asserts that an exit hop accepts an
+// incoming HTLC if its expiry is exactly at the maximum.
+func TestChannelLinkExitHopExpiryAtMaximum(t *testing.T) {
+	t.Parallel()
+
+	const chanAmt = btcutil.SatoshiPerBitcoin * 5
+	harness, err := newSingleLinkTestHarness(t, chanAmt, 0)
+	require.NoError(t, err, "unable to create link")
+
+	if err := harness.start(); err != nil {
+		t.Fatalf("unable to start test harness: %v", err)
+	}
+	t.Cleanup(harness.aliceLink.Stop)
+
+	coreLink, ok := harness.aliceLink.(*channelLink)
+	require.True(t, ok)
+
+	registry, ok := coreLink.cfg.Registry.(*mockInvoiceRegistry)
+	require.True(t, ok)
+
+	alicePeer, ok := coreLink.cfg.Peer.(*mockPeer)
+	require.True(t, ok)
+	aliceMsgs := alicePeer.sentMsgs
+
+	registry.settleChan = make(chan lntypes.Hash)
+
+	htlc, invoice := generateHtlcAndInvoice(t, 0)
+	htlc.Expiry = testStartingHeight +
+		invpkg.MaxFinalCltvDelta
+
+	err = registry.AddInvoice(t.Context(), *invoice, htlc.PaymentHash)
+	require.NoError(t, err, "unable to add invoice to registry")
+
+	ctx := linkTestContext{
+		t:           t,
+		aliceSwitch: harness.aliceSwitch,
+		aliceLink:   harness.aliceLink,
+		aliceMsgs:   aliceMsgs,
+		bobChannel:  harness.bobChannel,
+	}
+
+	ctx.sendHtlcBobToAlice(htlc)
+	ctx.sendCommitSigBobToAlice(1)
+	ctx.receiveRevAndAckAliceToBob()
+	ctx.receiveCommitSigAliceToBob(1)
+	ctx.sendRevAndAckBobToAlice()
+
+	select {
+	case <-registry.settleChan:
+	case <-time.After(5 * time.Second):
+		t.Fatal("expected exit hop notification")
+	}
+
+	ctx.receiveSettleAliceToBob()
+	ctx.receiveCommitSigAliceToBob(0)
 }
 
 // TestChannelLinkRevocationWindowRegular asserts that htlcs paying to a regular

lnrpc/invoicesrpc/addinvoice_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/btcsuite/btcd/wire/v2"
 	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/graph/db/models"
+	"github.com/lightningnetwork/lnd/invoices"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/routing/route"
 	"github.com/lightningnetwork/lnd/zpay32"
@@ -26,6 +27,24 @@ var (
 	_       = pubKeyY.SetByteSlice(pubkeyBytes)
 	pubkey  = btcec.NewPublicKey(new(btcec.FieldVal).SetInt(4), pubKeyY)
 )
+
+// TestAddInvoiceRejectsCltvAboveMaxIncoming asserts that invoice creation
+// rejects final CLTV deltas above the supported maximum.
+func TestAddInvoiceRejectsCltvAboveMaxIncoming(t *testing.T) {
+	t.Parallel()
+
+	_, _, err := AddInvoice(
+		t.Context(), &AddInvoiceConfig{}, &AddInvoiceData{
+			CltvExpiry: invoices.MaxFinalCltvDelta + 1,
+		},
+	)
+	require.ErrorContains(
+		t, err, fmt.Sprintf(
+			"max accepted is: %v",
+			invoices.MaxFinalCltvDelta,
+		),
+	)
+}
 
 type hopHintsConfigMock struct {
 	t *testing.T

flokiorg/flnd

htlcswitch/hop/forwarding_info.go
@@ -34,3 +34,52 @@ type ForwardingInfo struct {
 	// correct context.
 	PathID *chainhash.Hash
 }
+
+// FinalHtlcValidationResult describes the result of checking a final-hop
+// HTLC against the onion payload and supported final-hop CLTV range.
+type FinalHtlcValidationResult uint8
+
+const (
+	// FinalHtlcValid indicates that the HTLC matches the final-hop payload
+	// and supported final-hop CLTV range.
+	FinalHtlcValid FinalHtlcValidationResult = iota
+
+	// FinalHtlcInvalidAmount indicates that the HTLC amount is below the
+	// final amount requested by the onion payload.
+	FinalHtlcInvalidAmount
+
+	// FinalHtlcInvalidCltv indicates that the HTLC expiry is below the
+	// final CLTV requested by the onion payload.
+	FinalHtlcInvalidCltv
+
+	// FinalHtlcExpiryTooFar indicates that the HTLC expiry is outside the
+	// supported final-hop CLTV range.
+	FinalHtlcExpiryTooFar
+)
+
+// ValidateFinalHtlc checks final-hop HTLC amount and CLTV details before
+// invoice resolution.
+func ValidateFinalHtlc(amt lnwire.MilliLoki, expiry, heightNow,
+	maxFinalCltvDelta uint32, fwdInfo ForwardingInfo,
+	validateAmount bool) FinalHtlcValidationResult {
+
+	switch {
+	// The HTLC amount is below the final amount requested by the
+	// onion payload.
+	case validateAmount && amt < fwdInfo.AmountToForward:
+		return FinalHtlcInvalidAmount
+
+	// The HTLC expiry is below the final CLTV requested by the onion
+	// payload.
+	case expiry < fwdInfo.OutgoingCTLV:
+		return FinalHtlcInvalidCltv
+
+	// The HTLC expiry is outside the supported final-hop CLTV range.
+	case expiry > heightNow && expiry-heightNow > maxFinalCltvDelta:
+
+		return FinalHtlcExpiryTooFar
+
+	default:
+		return FinalHtlcValid
+	}
+}

htlcswitch/hop/forwarding_info_test.go
@@ -1,0 +1,137 @@
+package hop
+
+import (
+	"testing"
+
+	"github.com/flokiorg/flnd/lnwire"
+	"github.com/stretchr/testify/require"
+)
+
+// TestValidateFinalHtlc exercises the final-hop HTLC validation helper.
+func TestValidateFinalHtlc(t *testing.T) {
+	t.Parallel()
+
+	const (
+		amount       = lnwire.MilliLoki(1000)
+		expiry       = uint32(150)
+		height       = uint32(100)
+		maxCltvDelta = uint32(50)
+	)
+
+	fwdInfo := ForwardingInfo{
+		AmountToForward: amount,
+		OutgoingCTLV:    expiry,
+		NextHop:         Exit,
+	}
+
+	testCases := []struct {
+		name           string
+		amount         lnwire.MilliLoki
+		expiry         uint32
+		height         uint32
+		maxCltvDelta   uint32
+		fwdInfo        ForwardingInfo
+		validateAmount bool
+		expected       FinalHtlcValidationResult
+	}{{
+		name:           "valid",
+		amount:         amount,
+		expiry:         expiry,
+		height:         height + 1,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcValid,
+	}, {
+		name:           "amount too low",
+		amount:         amount - 1,
+		expiry:         expiry,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcInvalidAmount,
+	}, {
+		name:           "amount check disabled",
+		amount:         amount - 1,
+		expiry:         expiry,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: false,
+		expected:       FinalHtlcValid,
+	}, {
+		name:           "final cltv too low",
+		amount:         amount,
+		expiry:         expiry - 1,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcInvalidCltv,
+	}, {
+		name:           "expiry too far",
+		amount:         amount,
+		expiry:         expiry + 1,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcExpiryTooFar,
+	}, {
+		name:           "expiry at maximum",
+		amount:         amount,
+		expiry:         expiry,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcValid,
+	}, {
+		name:           "height above expiry",
+		amount:         amount,
+		expiry:         expiry,
+		height:         expiry + 1,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcValid,
+	}, {
+		name:           "amount failure takes precedence",
+		amount:         amount - 1,
+		expiry:         expiry - 1,
+		height:         height,
+		maxCltvDelta:   maxCltvDelta,
+		fwdInfo:        fwdInfo,
+		validateAmount: true,
+		expected:       FinalHtlcInvalidAmount,
+	}, {
+		name: "cltv failure takes precedence over " +
+			"expiry too far",
+		amount:       amount,
+		expiry:       expiry + maxCltvDelta + 1,
+		height:       height,
+		maxCltvDelta: maxCltvDelta,
+		fwdInfo: ForwardingInfo{
+			AmountToForward: amount,
+			OutgoingCTLV:    expiry + maxCltvDelta + 2,
+			NextHop:         Exit,
+		},
+		validateAmount: true,
+		expected:       FinalHtlcInvalidCltv,
+	}}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			t.Parallel()
+
+			result := ValidateFinalHtlc(
+				testCase.amount, testCase.expiry,
+				testCase.height, testCase.maxCltvDelta,
+				testCase.fwdInfo, testCase.validateAmount,
+			)
+
+			require.Equal(t, testCase.expected, result)
+		})
+	}
+}

htlcswitch/link_test.go
@@ -6301,14 +6301,48 @@ func TestCheckHtlcForward(t *testing.T) {
 
 	})
 
-	t.Run("cltv expiry too far in the future", func(t *testing.T) {
-		// Check that expiry isn't too far in the future.
+	t.Run("cltv expiry outside supported range", func(t *testing.T) {
+		// Check that expiry stays within the supported range.
 		result := link.CheckHtlcForward(
 			hash, 1500, 1000, 10200, 10100, models.InboundFee{}, 0,
 			lnwire.ShortChannelID{}, nil,
 		)
+		_, ok := result.WireMessage().(*lnwire.FailExpiryTooFar)
+		if !ok {
+			t.Fatalf("expected FailExpiryTooFar failure code")
+		}
+	})
+
+	t.Run("incoming cltv delta outside range", func(t *testing.T) {
+		result := link.CheckHtlcForward(
+			hash, 1500, 1000, 150+DefaultMaxOutgoingCltvExpiry+1,
+			150, models.InboundFee{}, 0, lnwire.ShortChannelID{},
+			nil,
+		)
 		if _, ok := result.WireMessage().(*lnwire.FailExpiryTooFar); !ok {
 			t.Fatalf("expected FailExpiryTooFar failure code")
+		}
+	})
+
+	t.Run("incoming cltv delta at maximum", func(t *testing.T) {
+		result := link.CheckHtlcForward(
+			hash, 1500, 1000, 150+DefaultMaxOutgoingCltvExpiry,
+			150, models.InboundFee{}, 0, lnwire.ShortChannelID{},
+			nil,
+		)
+		require.Nil(t, result)
+	})
+
+	t.Run("incoming cltv below outgoing cltv", func(t *testing.T) {
+		result := link.CheckHtlcForward(
+			hash, 1500, 1000, 190, 200, models.InboundFee{}, 0,
+			lnwire.ShortChannelID{}, nil,
+		)
+		_, ok := result.WireMessage().(*lnwire.FailIncorrectCltvExpiry)
+		if !ok {
+			t.Fatalf(
+				"expected FailIncorrectCltvExpiry failure code",
+			)
 		}
 	})
 
@@ -6641,6 +6675,121 @@ func TestChannelLinkHoldInvoiceRestart(t *testing.T) {
 		t.Fatalf("did not expect message %T", msg)
 	default:
 	}
+}
+
+// TestChannelLinkExitHopExpiryTooFar asserts that an exit hop fails an
+// incoming HTLC if its expiry is outside the supported range.
+func TestChannelLinkExitHopExpiryTooFar(t *testing.T) {
+	t.Parallel()
+
+	const chanAmt = chainutil.LokiPerFlokicoin * 5
+	harness, err := newSingleLinkTestHarness(t, chanAmt, 0)
+	require.NoError(t, err, "unable to create link")
+
+	if err := harness.start(); err != nil {
+		t.Fatalf("unable to start test harness: %v", err)
+	}
+	t.Cleanup(harness.aliceLink.Stop)
+
+	coreLink, ok := harness.aliceLink.(*channelLink)
+	require.True(t, ok)
+
+	registry, ok := coreLink.cfg.Registry.(*mockInvoiceRegistry)
+	require.True(t, ok)
+
+	alicePeer, ok := coreLink.cfg.Peer.(*mockPeer)
+	require.True(t, ok)
+	aliceMsgs := alicePeer.sentMsgs
+
+	registry.settleChan = make(chan lntypes.Hash)
+
+	htlc, invoice := generateHtlcAndInvoice(t, 0)
+	htlc.Expiry = testStartingHeight +
+		invpkg.MaxFinalCltvDelta + 1
+
+	err = registry.AddInvoice(t.Context(), *invoice, htlc.PaymentHash)
+	require.NoError(t, err, "unable to add invoice to registry")
+
+	ctx := linkTestContext{
+		t:           t,
+		aliceSwitch: harness.aliceSwitch,
+		aliceLink:   harness.aliceLink,
+		aliceMsgs:   aliceMsgs,
+		bobChannel:  harness.bobChannel,
+	}
+
+	ctx.sendHtlcBobToAlice(htlc)
+	ctx.sendCommitSigBobToAlice(1)
+	ctx.receiveRevAndAckAliceToBob()
+	ctx.receiveCommitSigAliceToBob(1)
+	ctx.sendRevAndAckBobToAlice()
+	ctx.receiveFailAliceToBobWithCode(
+		lnwire.CodeIncorrectOrUnknownPaymentDetails,
+	)
+	ctx.receiveCommitSigAliceToBob(0)
+
+	select {
+	case <-registry.settleChan:
+		t.Fatal("exit hop notification received")
+	case <-time.After(time.Second):
+	}
+}
+
+// TestChannelLinkExitHopExpiryAtMaximum asserts that an exit hop accepts an
+// incoming HTLC if its expiry is exactly at the maximum.
+func TestChannelLinkExitHopExpiryAtMaximum(t *testing.T) {
+	t.Parallel()
+
+	const chanAmt = chainutil.LokiPerFlokicoin * 5
+	harness, err := newSingleLinkTestHarness(t, chanAmt, 0)
+	require.NoError(t, err, "unable to create link")
+
+	if err := harness.start(); err != nil {
+		t.Fatalf("unable to start test harness: %v", err)
+	}
+	t.Cleanup(harness.aliceLink.Stop)
+
+	coreLink, ok := harness.aliceLink.(*channelLink)
+	require.True(t, ok)
+
+	registry, ok := coreLink.cfg.Registry.(*mockInvoiceRegistry)
+	require.True(t, ok)
+
+	alicePeer, ok := coreLink.cfg.Peer.(*mockPeer)
+	require.True(t, ok)
+	aliceMsgs := alicePeer.sentMsgs
+
+	registry.settleChan = make(chan lntypes.Hash)
+
+	htlc, invoice := generateHtlcAndInvoice(t, 0)
+	htlc.Expiry = testStartingHeight +
+		invpkg.MaxFinalCltvDelta
+
+	err = registry.AddInvoice(t.Context(), *invoice, htlc.PaymentHash)
+	require.NoError(t, err, "unable to add invoice to registry")
+
+	ctx := linkTestContext{
+		t:           t,
+		aliceSwitch: harness.aliceSwitch,
+		aliceLink:   harness.aliceLink,
+		aliceMsgs:   aliceMsgs,
+		bobChannel:  harness.bobChannel,
+	}
+
+	ctx.sendHtlcBobToAlice(htlc)
+	ctx.sendCommitSigBobToAlice(1)
+	ctx.receiveRevAndAckAliceToBob()
+	ctx.receiveCommitSigAliceToBob(1)
+	ctx.sendRevAndAckBobToAlice()
+
+	select {
+	case <-registry.settleChan:
+	case <-time.After(5 * time.Second):
+		t.Fatal("expected exit hop notification")
+	}
+
+	ctx.receiveSettleAliceToBob()
+	ctx.receiveCommitSigAliceToBob(0)
 }
 
 // TestChannelLinkRevocationWindowRegular asserts that htlcs paying to a regular

lnrpc/invoicesrpc/addinvoice_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/flokiorg/flnd/chanstate"
 	"github.com/flokiorg/flnd/graph/db/models"
+	"github.com/flokiorg/flnd/invoices"
 	"github.com/flokiorg/flnd/lnwire"
 	"github.com/flokiorg/flnd/zpay32"
 	"github.com/flokiorg/go-flokicoin/crypto"
@@ -24,6 +25,24 @@ var (
 	_       = pubKeyY.SetByteSlice(pubkeyBytes)
 	pubkey  = crypto.NewPublicKey(new(crypto.FieldVal).SetInt(4), pubKeyY)
 )
+
+// TestAddInvoiceRejectsCltvAboveMaxIncoming asserts that invoice creation
+// rejects final CLTV deltas above the supported maximum.
+func TestAddInvoiceRejectsCltvAboveMaxIncoming(t *testing.T) {
+	t.Parallel()
+
+	_, _, err := AddInvoice(
+		t.Context(), &AddInvoiceConfig{}, &AddInvoiceData{
+			CltvExpiry: invoices.MaxFinalCltvDelta + 1,
+		},
+	)
+	require.ErrorContains(
+		t, err, fmt.Sprintf(
+			"max accepted is: %v",
+			invoices.MaxFinalCltvDelta,
+		),
+	)
+}
 
 type hopHintsConfigMock struct {
 	t *testing.T