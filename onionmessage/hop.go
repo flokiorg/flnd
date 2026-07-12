gijswijs/lnd

multi: OnionPeerActor for per-peer message support
Introduce a fat OnionPeerActor that handles the full onion message
processing pipeline for each peer connection. The actor decodes incoming
onion messages, determines the routing action (forward or deliver),
executes the action via PeerMessageSender, and dispatches updates to
subscribers via OnionMessageUpdateDispatcher.

Key components:
- OnionRouter interface abstracting sphinx router operations
- PeerMessageSender interface for forwarding to other peers
- OnionMessageUpdateDispatcher interface for subscriber notifications
- OnionActorFactory for spawning per-peer actors with shared deps
- Full test suite calling Receive() directly with NoOpReplayLog

onionmessage/hop.go
@@ -1,0 +1,191 @@
+package onionmessage
+
+import (
+	"bytes"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	sphinx "github.com/lightningnetwork/lightning-onion"
+	"github.com/lightningnetwork/lnd/fn/v2"
+	"github.com/lightningnetwork/lnd/lnwire"
+	"github.com/lightningnetwork/lnd/record"
+	"github.com/lightningnetwork/lnd/tlv"
+)
+
+// forwardAction contains the information needed to forward an onion message to
+// the next node as well as update any subscribers with the payload we received.
+type forwardAction struct {
+	// nextNodeID is the public key of the peer to forward the message to
+	nextNodeID *btcec.PublicKey
+
+	// nextPathKey is the path key for the next hop, used for route
+	// blinding.
+	nextPathKey *btcec.PublicKey
+
+	// nextPacket is the serialized onion packet to send to the next hop.
+	nextPacket []byte
+
+	// payload contains the decoded payload for this hop, which may include
+	// custom records and routing information.
+	payload *lnwire.OnionMessagePayload
+}
+
+// deliverAction contains the information needed to deliver the payload to any
+// subscribers. Since we only support forwarding onion messages, this is only
+// needed in itest to verify correct handling and behavior.
+type deliverAction struct {
+	// payload contains the decoded payload for this hop, which may include
+	// custom records and routing information.
+	payload *lnwire.OnionMessagePayload
+}
+
+type routingAction = fn.Either[forwardAction, deliverAction]
+
+// NodeIDResolver defines an interface to resolve a node public key from a short
+// channel ID.
+type NodeIDResolver interface {
+	RemotePubFromSCID(scid lnwire.ShortChannelID) (*btcec.PublicKey, error)
+}
+
+// processOnionMessage decodes and processes an onion message packet and its
+// contents. It assumes route blinding is used, so it also decrypts encrypted
+// recipient data, and derives the next path key. It returns a fn.Result type
+// containing a routingAction, which contains all the information required to
+// execute the next step in the routing process.
+func processOnionMessage(router OnionRouter, resolver NodeIDResolver,
+	msg *lnwire.OnionMessage) fn.Result[routingAction] {
+
+	var onionPkt sphinx.OnionPacket
+	err := onionPkt.Decode(bytes.NewReader(msg.OnionBlob))
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	// TODO(gijs): We should not use the magic value 10 here. It's the
+	// incomingCltv value and only has use for the replay protection that we
+	// don't need anyway.
+	processedPkt, err := router.ProcessOnionPacket(
+		&onionPkt, nil, 10, sphinx.WithBlindingPoint(msg.PathKey),
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	payload := lnwire.NewOnionMessagePayload()
+	_, err = payload.Decode(
+		bytes.NewReader(processedPkt.Payload.Payload),
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	// Create a shallow copy of the payload but deep copy the EncryptedData
+	// field, as the decryption below will overwrite the EncryptedData field
+	// in-place.
+	originalPayload := *payload
+	originalPayload.EncryptedData = bytes.Clone(payload.EncryptedData)
+
+	decrypted, err := router.DecryptBlindedHopData(
+		msg.PathKey, payload.EncryptedData,
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	routeData, err := record.DecodeBlindedRouteData(
+		bytes.NewReader(decrypted),
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	nextPathKey := deriveNextPathKey(router, msg.PathKey,
+		routeData.NextBlindingOverride)
+
+	action, err := createRoutingAction(
+		resolver, processedPkt, &originalPayload, routeData,
+		nextPathKey,
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	return fn.Ok(action)
+}
+
+// createRoutingAction creates the routing action based on whether we are
+// forwarding or the receiver of the onion message.
+func createRoutingAction(resolver NodeIDResolver,
+	packet *sphinx.ProcessedPacket, payload *lnwire.OnionMessagePayload,
+	routeData *record.BlindedRouteData,
+	nextPathKey *btcec.PublicKey) (routingAction, error) {
+
+	if isForwarding(packet) {
+		var nextNodeID *btcec.PublicKey
+		if routeData.NextNodeID.IsSome() {
+			n, err := routeData.NextNodeID.UnwrapOrErr(
+				ErrNextNodeIdEmpty,
+			)
+			if err != nil {
+				return routingAction{}, err
+			}
+			nextNodeID = n.Val
+		} else {
+			scid, err := routeData.ShortChannelID.UnwrapOrErr(
+				ErrSCIDEmpty,
+			)
+			if err != nil {
+				return routingAction{}, err
+			}
+			nextNodeID, err = resolver.RemotePubFromSCID(scid.Val)
+			if err != nil {
+				return routingAction{}, err
+			}
+		}
+
+		buf := new(bytes.Buffer)
+		err := packet.NextPacket.Encode(buf)
+		if err != nil {
+			return routingAction{}, err
+		}
+		nextPacket := buf.Bytes()
+
+		return fn.NewLeft[forwardAction, deliverAction](forwardAction{
+			nextNodeID:  nextNodeID,
+			nextPathKey: nextPathKey,
+			nextPacket:  nextPacket,
+			payload:     payload,
+		}), nil
+	}
+
+	return fn.NewRight[forwardAction](deliverAction{
+		payload: payload,
+	}), nil
+}
+
+// deriveNextPathKey derives the next path key using the router and current
+// path key. If an override is provided, it is used instead.
+func deriveNextPathKey(router OnionRouter, currentPathKey *btcec.PublicKey,
+	override tlv.OptionalRecordT[tlv.TlvType8,
+		*btcec.PublicKey]) *btcec.PublicKey {
+
+	// If an override is provided, use it.
+	return override.UnwrapOrFunc(func() tlv.RecordT[tlv.TlvType8,
+		*btcec.PublicKey] {
+
+		// Otherwise, derive the next path key using the router.
+		nextKey, err := router.NextEphemeral(currentPathKey)
+		if err != nil {
+			// If the derivation fails, log and return a zero key.
+			log.Warnf("Failed to derive next path key: %v", err)
+
+			return override.Zero()
+		}
+
+		return tlv.NewPrimitiveRecord[tlv.TlvType8](nextKey)
+	}).Val
+}
+
+// isForwarding checks if the packet is to be forwarded or delivered.
+func isForwarding(packet *sphinx.ProcessedPacket) bool {
+	return packet.Action != sphinx.ExitNode
+}

onionmessage/interfaces.go
@@ -1,0 +1,41 @@
+package onionmessage
+
+import (
+	"github.com/btcsuite/btcd/btcec/v2"
+	sphinx "github.com/lightningnetwork/lightning-onion"
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// OnionRouter wraps the sphinx router operations needed for onion message
+// processing.
+type OnionRouter interface {
+	// ProcessOnionPacket processes an onion packet and returns the
+	// processed result.
+	ProcessOnionPacket(pkt *sphinx.OnionPacket, assocData []byte,
+		incomingCltv uint32,
+		opts ...sphinx.ProcessOnionOpt) (*sphinx.ProcessedPacket, error)
+
+	// DecryptBlindedHopData decrypts the encrypted hop data using the
+	// given path key.
+	DecryptBlindedHopData(pathKey *btcec.PublicKey,
+		encData []byte) ([]byte, error)
+
+	// NextEphemeral derives the next ephemeral key from the current path
+	// key.
+	NextEphemeral(
+		currentPathKey *btcec.PublicKey) (*btcec.PublicKey, error)
+}
+
+// OnionMessageUpdateDispatcher dispatches onion message updates to
+// subscribers.
+type OnionMessageUpdateDispatcher interface {
+	// SendUpdate sends an onion message update to all subscribers.
+	SendUpdate(update any) error
+}
+
+// PeerMessageSender sends onion messages to peers identified by public key.
+type PeerMessageSender interface {
+	// SendToPeer sends an onion message to the peer identified by the
+	// given compressed public key.
+	SendToPeer(pubKey [33]byte, msg *lnwire.OnionMessage) error
+}

onionmessage/onion_endpoint.go
@@ -5,10 +5,12 @@ import (
 	"encoding/hex"
 	"log/slog"
 
+	sphinx "github.com/lightningnetwork/lightning-onion"
 	"github.com/btcsuite/btclog/v2"
 	"github.com/lightningnetwork/lnd/lnutils"
 	"github.com/lightningnetwork/lnd/lnwire"
 	"github.com/lightningnetwork/lnd/msgmux"
+	"github.com/lightningnetwork/lnd/record"
 	"github.com/lightningnetwork/lnd/subscribe"
 )
 
@@ -27,6 +29,19 @@ type OnionMessageUpdate struct {
 	// manner as onions used to route HTLCs, with the exception that it uses
 	// blinded routes by default.
 	OnionBlob []byte
+
+	// CustomRecords contains any custom TLV records included in the
+	// payload.
+	CustomRecords record.CustomSet
+
+	// ReplyPath contains the reply path information for the onion message.
+	ReplyPath *sphinx.BlindedPath
+
+	// EncryptedRecipientData contains the encrypted recipient data for the
+	// onion message, created by the creator of the blinded route. This is
+	// the receiver for the last leg of the route, and the sender for the
+	// first leg up to the introduction point.
+	EncryptedRecipientData []byte
 }
 
 // OnionEndpoint handles incoming onion messages.

routing/route/blindedroute.go
@@ -1,0 +1,87 @@
+package route
+
+import (
+	"fmt"
+
+	"github.com/btcsuite/btcd/btcec/v2"
+	sphinx "github.com/lightningnetwork/lightning-onion"
+	"github.com/lightningnetwork/lnd/lnwire"
+)
+
+// OnionMessageBlindedPathToSphinxPath converts a complete blinded path intended
+// for sending an onion message to a PaymentPath that contains the per-hop
+// payloads used to encoding the routing data for each hop in the route. This
+// method also accepts final hop payloads.
+func OnionMessageBlindedPathToSphinxPath(blindedPath *sphinx.BlindedPath,
+	replyPath *sphinx.BlindedPath, finalHopTLVs []*lnwire.FinalHopTLV) (
+	*sphinx.PaymentPath, error) {
+
+	var path sphinx.PaymentPath
+
+	// We can only construct a route if there are hops provided.
+	if len(blindedPath.BlindedHops) == 0 {
+		return nil, ErrNoRouteHopsProvided
+	}
+
+	// Check maximum route length. We keep the maximum the same as
+	// sphinx.NumMaxHops for simplicity. In theory the maximum for onion
+	// messages could be higher, namely 481. See:
+	// https://delvingbitcoin.org/t/onion-messaging-dos-threat-mitigations
+	if len(blindedPath.BlindedHops) > sphinx.NumMaxHops {
+		return nil, ErrMaxRouteHopsExceeded
+	}
+
+	// For each hop encoded within the route, we'll convert the hop struct
+	// to an OnionHop with matching per-hop payload within the path as used
+	// by the sphinx package.
+	for i, hop := range blindedPath.BlindedHops {
+		// Create an onionMessagePayload with the encrypted data for
+		// this hop.
+		onionMessagePayload := &lnwire.OnionMessagePayload{
+			EncryptedData: hop.CipherText,
+		}
+
+		// If we're on the final hop include the tlvs intended for the
+		// final hop and the reply path (if provided).
+		finalHop := i == len(blindedPath.BlindedHops)-1
+		if finalHop {
+			onionMessagePayload.FinalHopTLVs = finalHopTLVs
+			onionMessagePayload.ReplyPath = replyPath
+		}
+
+		// create a sphinx hop for this blinded hop.
+		hop, err := createSphinxHop(
+			*hop.BlindedNodePub, onionMessagePayload,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("sphinx hop %v: %w", i, err)
+		}
+		path[i] = *hop
+	}
+
+	return &path, nil
+}
+
+// createSphinxHop encodes an onion message payload and produces a sphinx
+// onion hop for it.
+func createSphinxHop(nodeID btcec.PublicKey,
+	onionMessagePayload *lnwire.OnionMessagePayload) (*sphinx.OnionHop,
+	error) {
+
+	encodeOnionMessagePayload, err := onionMessagePayload.Encode()
+	if err != nil {
+		return nil, fmt.Errorf("failed onion message payload encode: "+
+			"%w", err)
+	}
+
+	hopPayload, err := sphinx.NewTLVHopPayload(encodeOnionMessagePayload)
+	if err != nil {
+		return nil, fmt.Errorf("failed creation of tlv hop payload: "+
+			"%w", err)
+	}
+
+	return &sphinx.OnionHop{
+		NodePub:    nodeID,
+		HopPayload: hopPayload,
+	}, nil
+}

flokiorg/flnd

onionmessage/hop.go
@@ -1,0 +1,191 @@
+package onionmessage
+
+import (
+	"bytes"
+
+	"github.com/flokiorg/go-flokicoin/crypto"
+	sphinx "github.com/flokiorg/lightning-onion"
+	"github.com/flokiorg/flnd/fn"
+	"github.com/flokiorg/flnd/lnwire"
+	"github.com/flokiorg/flnd/record"
+	"github.com/flokiorg/flnd/tlv"
+)
+
+// forwardAction contains the information needed to forward an onion message to
+// the next node as well as update any subscribers with the payload we received.
+type forwardAction struct {
+	// nextNodeID is the public key of the peer to forward the message to
+	nextNodeID *crypto.PublicKey
+
+	// nextPathKey is the path key for the next hop, used for route
+	// blinding.
+	nextPathKey *crypto.PublicKey
+
+	// nextPacket is the serialized onion packet to send to the next hop.
+	nextPacket []byte
+
+	// payload contains the decoded payload for this hop, which may include
+	// custom records and routing information.
+	payload *lnwire.OnionMessagePayload
+}
+
+// deliverAction contains the information needed to deliver the payload to any
+// subscribers. Since we only support forwarding onion messages, this is only
+// needed in itest to verify correct handling and behavior.
+type deliverAction struct {
+	// payload contains the decoded payload for this hop, which may include
+	// custom records and routing information.
+	payload *lnwire.OnionMessagePayload
+}
+
+type routingAction = fn.Either[forwardAction, deliverAction]
+
+// NodeIDResolver defines an interface to resolve a node public key from a short
+// channel ID.
+type NodeIDResolver interface {
+	RemotePubFromSCID(scid lnwire.ShortChannelID) (*crypto.PublicKey, error)
+}
+
+// processOnionMessage decodes and processes an onion message packet and its
+// contents. It assumes route blinding is used, so it also decrypts encrypted
+// recipient data, and derives the next path key. It returns a fn.Result type
+// containing a routingAction, which contains all the information required to
+// execute the next step in the routing process.
+func processOnionMessage(router OnionRouter, resolver NodeIDResolver,
+	msg *lnwire.OnionMessage) fn.Result[routingAction] {
+
+	var onionPkt sphinx.OnionPacket
+	err := onionPkt.Decode(bytes.NewReader(msg.OnionBlob))
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	// TODO(gijs): We should not use the magic value 10 here. It's the
+	// incomingCltv value and only has use for the replay protection that we
+	// don't need anyway.
+	processedPkt, err := router.ProcessOnionPacket(
+		&onionPkt, nil, 10, sphinx.WithBlindingPoint(msg.PathKey),
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	payload := lnwire.NewOnionMessagePayload()
+	_, err = payload.Decode(
+		bytes.NewReader(processedPkt.Payload.Payload),
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	// Create a shallow copy of the payload but deep copy the EncryptedData
+	// field, as the decryption below will overwrite the EncryptedData field
+	// in-place.
+	originalPayload := *payload
+	originalPayload.EncryptedData = bytes.Clone(payload.EncryptedData)
+
+	decrypted, err := router.DecryptBlindedHopData(
+		msg.PathKey, payload.EncryptedData,
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	routeData, err := record.DecodeBlindedRouteData(
+		bytes.NewReader(decrypted),
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	nextPathKey := deriveNextPathKey(router, msg.PathKey,
+		routeData.NextBlindingOverride)
+
+	action, err := createRoutingAction(
+		resolver, processedPkt, &originalPayload, routeData,
+		nextPathKey,
+	)
+	if err != nil {
+		return fn.Err[routingAction](err)
+	}
+
+	return fn.Ok(action)
+}
+
+// createRoutingAction creates the routing action based on whether we are
+// forwarding or the receiver of the onion message.
+func createRoutingAction(resolver NodeIDResolver,
+	packet *sphinx.ProcessedPacket, payload *lnwire.OnionMessagePayload,
+	routeData *record.BlindedRouteData,
+	nextPathKey *crypto.PublicKey) (routingAction, error) {
+
+	if isForwarding(packet) {
+		var nextNodeID *crypto.PublicKey
+		if routeData.NextNodeID.IsSome() {
+			n, err := routeData.NextNodeID.UnwrapOrErr(
+				ErrNextNodeIdEmpty,
+			)
+			if err != nil {
+				return routingAction{}, err
+			}
+			nextNodeID = n.Val
+		} else {
+			scid, err := routeData.ShortChannelID.UnwrapOrErr(
+				ErrSCIDEmpty,
+			)
+			if err != nil {
+				return routingAction{}, err
+			}
+			nextNodeID, err = resolver.RemotePubFromSCID(scid.Val)
+			if err != nil {
+				return routingAction{}, err
+			}
+		}
+
+		buf := new(bytes.Buffer)
+		err := packet.NextPacket.Encode(buf)
+		if err != nil {
+			return routingAction{}, err
+		}
+		nextPacket := buf.Bytes()
+
+		return fn.NewLeft[forwardAction, deliverAction](forwardAction{
+			nextNodeID:  nextNodeID,
+			nextPathKey: nextPathKey,
+			nextPacket:  nextPacket,
+			payload:     payload,
+		}), nil
+	}
+
+	return fn.NewRight[forwardAction](deliverAction{
+		payload: payload,
+	}), nil
+}
+
+// deriveNextPathKey derives the next path key using the router and current
+// path key. If an override is provided, it is used instead.
+func deriveNextPathKey(router OnionRouter, currentPathKey *crypto.PublicKey,
+	override tlv.OptionalRecordT[tlv.TlvType8,
+		*crypto.PublicKey]) *crypto.PublicKey {
+
+	// If an override is provided, use it.
+	return override.UnwrapOrFunc(func() tlv.RecordT[tlv.TlvType8,
+		*crypto.PublicKey] {
+
+		// Otherwise, derive the next path key using the router.
+		nextKey, err := router.NextEphemeral(currentPathKey)
+		if err != nil {
+			// If the derivation fails, log and return a zero key.
+			log.Warnf("Failed to derive next path key: %v", err)
+
+			return override.Zero()
+		}
+
+		return tlv.NewPrimitiveRecord[tlv.TlvType8](nextKey)
+	}).Val
+}
+
+// isForwarding checks if the packet is to be forwarded or delivered.
+func isForwarding(packet *sphinx.ProcessedPacket) bool {
+	return packet.Action != sphinx.ExitNode
+}

onionmessage/interfaces.go
@@ -1,0 +1,41 @@
+package onionmessage
+
+import (
+	"github.com/flokiorg/go-flokicoin/crypto"
+	sphinx "github.com/flokiorg/lightning-onion"
+	"github.com/flokiorg/flnd/lnwire"
+)
+
+// OnionRouter wraps the sphinx router operations needed for onion message
+// processing.
+type OnionRouter interface {
+	// ProcessOnionPacket processes an onion packet and returns the
+	// processed result.
+	ProcessOnionPacket(pkt *sphinx.OnionPacket, assocData []byte,
+		incomingCltv uint32,
+		opts ...sphinx.ProcessOnionOpt) (*sphinx.ProcessedPacket, error)
+
+	// DecryptBlindedHopData decrypts the encrypted hop data using the
+	// given path key.
+	DecryptBlindedHopData(pathKey *crypto.PublicKey,
+		encData []byte) ([]byte, error)
+
+	// NextEphemeral derives the next ephemeral key from the current path
+	// key.
+	NextEphemeral(
+		currentPathKey *crypto.PublicKey) (*crypto.PublicKey, error)
+}
+
+// OnionMessageUpdateDispatcher dispatches onion message updates to
+// subscribers.
+type OnionMessageUpdateDispatcher interface {
+	// SendUpdate sends an onion message update to all subscribers.
+	SendUpdate(update any) error
+}
+
+// PeerMessageSender sends onion messages to peers identified by public key.
+type PeerMessageSender interface {
+	// SendToPeer sends an onion message to the peer identified by the
+	// given compressed public key.
+	SendToPeer(pubKey [33]byte, msg *lnwire.OnionMessage) error
+}

onionmessage/onion_endpoint.go
@@ -5,9 +5,11 @@ import (
 	"encoding/hex"
 	"log/slog"
 
+	sphinx "github.com/flokiorg/lightning-onion"
 	"github.com/flokiorg/flnd/lnutils"
 	"github.com/flokiorg/flnd/lnwire"
 	"github.com/flokiorg/flnd/msgmux"
+	"github.com/flokiorg/flnd/record"
 	"github.com/flokiorg/flnd/subscribe"
 	flog "github.com/flokiorg/go-flokicoin/log/v2"
 )
@@ -27,6 +29,19 @@ type OnionMessageUpdate struct {
 	// manner as onions used to route HTLCs, with the exception that it uses
 	// blinded routes by default.
 	OnionBlob []byte
+
+	// CustomRecords contains any custom TLV records included in the
+	// payload.
+	CustomRecords record.CustomSet
+
+	// ReplyPath contains the reply path information for the onion message.
+	ReplyPath *sphinx.BlindedPath
+
+	// EncryptedRecipientData contains the encrypted recipient data for the
+	// onion message, created by the creator of the blinded route. This is
+	// the receiver for the last leg of the route, and the sender for the
+	// first leg up to the introduction point.
+	EncryptedRecipientData []byte
 }
 
 // OnionEndpoint handles incoming onion messages.

routing/route/blindedroute.go
@@ -1,0 +1,87 @@
+package route
+
+import (
+	"fmt"
+
+	"github.com/flokiorg/go-flokicoin/crypto"
+	sphinx "github.com/flokiorg/lightning-onion"
+	"github.com/flokiorg/flnd/lnwire"
+)
+
+// OnionMessageBlindedPathToSphinxPath converts a complete blinded path intended
+// for sending an onion message to a PaymentPath that contains the per-hop
+// payloads used to encoding the routing data for each hop in the route. This
+// method also accepts final hop payloads.
+func OnionMessageBlindedPathToSphinxPath(blindedPath *sphinx.BlindedPath,
+	replyPath *sphinx.BlindedPath, finalHopTLVs []*lnwire.FinalHopTLV) (
+	*sphinx.PaymentPath, error) {
+
+	var path sphinx.PaymentPath
+
+	// We can only construct a route if there are hops provided.
+	if len(blindedPath.BlindedHops) == 0 {
+		return nil, ErrNoRouteHopsProvided
+	}
+
+	// Check maximum route length. We keep the maximum the same as
+	// sphinx.NumMaxHops for simplicity. In theory the maximum for onion
+	// messages could be higher, namely 481. See:
+	// https://delvingbitcoin.org/t/onion-messaging-dos-threat-mitigations
+	if len(blindedPath.BlindedHops) > sphinx.NumMaxHops {
+		return nil, ErrMaxRouteHopsExceeded
+	}
+
+	// For each hop encoded within the route, we'll convert the hop struct
+	// to an OnionHop with matching per-hop payload within the path as used
+	// by the sphinx package.
+	for i, hop := range blindedPath.BlindedHops {
+		// Create an onionMessagePayload with the encrypted data for
+		// this hop.
+		onionMessagePayload := &lnwire.OnionMessagePayload{
+			EncryptedData: hop.CipherText,
+		}
+
+		// If we're on the final hop include the tlvs intended for the
+		// final hop and the reply path (if provided).
+		finalHop := i == len(blindedPath.BlindedHops)-1
+		if finalHop {
+			onionMessagePayload.FinalHopTLVs = finalHopTLVs
+			onionMessagePayload.ReplyPath = replyPath
+		}
+
+		// create a sphinx hop for this blinded hop.
+		hop, err := createSphinxHop(
+			*hop.BlindedNodePub, onionMessagePayload,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("sphinx hop %v: %w", i, err)
+		}
+		path[i] = *hop
+	}
+
+	return &path, nil
+}
+
+// createSphinxHop encodes an onion message payload and produces a sphinx
+// onion hop for it.
+func createSphinxHop(nodeID crypto.PublicKey,
+	onionMessagePayload *lnwire.OnionMessagePayload) (*sphinx.OnionHop,
+	error) {
+
+	encodeOnionMessagePayload, err := onionMessagePayload.Encode()
+	if err != nil {
+		return nil, fmt.Errorf("failed onion message payload encode: "+
+			"%w", err)
+	}
+
+	hopPayload, err := sphinx.NewTLVHopPayload(encodeOnionMessagePayload)
+	if err != nil {
+		return nil, fmt.Errorf("failed creation of tlv hop payload: "+
+			"%w", err)
+	}
+
+	return &sphinx.OnionHop{
+		NodePub:    nodeID,
+		HopPayload: hopPayload,
+	}, nil
+}