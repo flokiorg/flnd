ziggie1984/lnd

routerrpc: implement DeleteForwardingHistory RPC handler
In this commit, we implement the server-side handler for the
DeleteForwardingHistory RPC, connecting the proto definition to the
database layer through the ForwardingLogDB interface on RouterBackend.

The handler resolves the time specification from the request oneof: an
absolute Unix timestamp is used directly, while a relative duration
string is parsed via parseDuration and resolved against the current
clock time. We use the injected clock (RouterBackend.Clock) rather than
time.Now to keep the handler testable.

A configurable minimum age guard (MinFwdHistoryAge, defaulting to 1h)
prevents accidental deletion of recent events. The minimum age can be
overridden via --routerrpc.min-fwd-history-age for environments such as
integration tests that need a shorter threshold.

The context is threaded through to DeleteForwardingEvents so that client
cancellation or deadline expiry aborts the deletion between batches.
Batches already committed at cancellation time are permanent, but the
operation is safe to re-run since deletion is idempotent.

lnrpc/routerrpc/router_backend.go
@@ -15,6 +15,7 @@ import (
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcd/wire"
 	sphinx "github.com/lightningnetwork/lightning-onion"
+	"github.com/lightningnetwork/lnd/channeldb"
 	"github.com/lightningnetwork/lnd/clock"
 	"github.com/lightningnetwork/lnd/feature"
 	"github.com/lightningnetwork/lnd/fn/v2"
@@ -129,6 +130,30 @@ type RouterBackend struct {
 	// Clock is the clock used to validate payment requests expiry.
 	// It is useful for testing.
 	Clock clock.Clock
+
+	// ForwardingLog provides access to forwarding log database operations.
+	ForwardingLog ForwardingLogDB
+
+	// MinForwardingHistoryAge is the minimum age a forwarding event must
+	// have before it can be deleted. If zero the handler defaults to 1
+	// hour.
+	MinForwardingHistoryAge time.Duration
+}
+
+// ForwardingLogDB defines the interface for forwarding log database operations.
+// This interface allows the router RPC to interact with the forwarding log
+// without depending directly on the channeldb implementation, making testing
+// and future refactoring easier.
+type ForwardingLogDB interface {
+	// DeleteForwardingEvents deletes all forwarding events with a
+	// timestamp at or before the specified endTime. The deletion is
+	// performed in batches of the given size to avoid holding large
+	// database locks. It returns statistics about the deletion including
+	// the number of events deleted and the total fees earned from those
+	// events. If the context is cancelled between batches, partial
+	// statistics are returned along with the context error.
+	DeleteForwardingEvents(ctx context.Context, endTime time.Time,
+		batchSize int) (channeldb.DeleteStats, error)
 }
 
 // MissionControl defines the mission control dependencies of routerrpc.

flokiorg/flnd

lnrpc/routerrpc/router_backend.go
@@ -10,6 +10,7 @@ import (
 	math "math"
 	"time"
 
+	"github.com/flokiorg/flnd/channeldb"
 	"github.com/flokiorg/flnd/clock"
 	"github.com/flokiorg/flnd/feature"
 	"github.com/flokiorg/flnd/fn"
@@ -129,6 +130,30 @@ type RouterBackend struct {
 	// Clock is the clock used to validate payment requests expiry.
 	// It is useful for testing.
 	Clock clock.Clock
+
+	// ForwardingLog provides access to forwarding log database operations.
+	ForwardingLog ForwardingLogDB
+
+	// MinForwardingHistoryAge is the minimum age a forwarding event must
+	// have before it can be deleted. If zero the handler defaults to 1
+	// hour.
+	MinForwardingHistoryAge time.Duration
+}
+
+// ForwardingLogDB defines the interface for forwarding log database operations.
+// This interface allows the router RPC to interact with the forwarding log
+// without depending directly on the channeldb implementation, making testing
+// and future refactoring easier.
+type ForwardingLogDB interface {
+	// DeleteForwardingEvents deletes all forwarding events with a
+	// timestamp at or before the specified endTime. The deletion is
+	// performed in batches of the given size to avoid holding large
+	// database locks. It returns statistics about the deletion including
+	// the number of events deleted and the total fees earned from those
+	// events. If the context is cancelled between batches, partial
+	// statistics are returned along with the context error.
+	DeleteForwardingEvents(ctx context.Context, endTime time.Time,
+		batchSize int) (channeldb.DeleteStats, error)
 }
 
 // MissionControl defines the mission control dependencies of routerrpc.